5045d232e8ef72756779ca6ce5ee71490ac0d8d6
Mitali Lakhere <lakheremitali01@gmail.com>
2022-09-19 17:22:46 +0530

    Add index.js file

--- app.js
+++ app.js
@@ -1,75 +1,47 @@
 const axios = require("axios");
 const express = require("express");
 const bodyParser = require("body-parser");
-const { response } = require("express");
+// const { response } = require("express");
 const app = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 // 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
-// app.get("/api",async(req,res)=>{
-//     let config={
-//         method:"get",
-//         url:`https://xdc.blocksscan.io/api/accounts/${req.query.address}`,
-
-//     };
-//    const ans1= axios(config)
-
-//     try{
-//         const res1 = await app.get('url');
-//         let ans={
-//             "status":"1",
-//             "message":"OK",
-//             "result":ans1.balance
-//         }
-
-//         res.send(ans);
-
-//     }
-
-//    catch(error){
-//     console.log(error);
-//    };
-// });
-
 
 async function sendRequest(address) {
+    try {
     let config = {
         method: "get",
         url: `https://xdc.blocksscan.io/api/accounts/${address}`,
     };
-    axios(config)
-    
-        .then(function (response) {
-           let ans = {
-                "status": "1",
-                "message": "OK",
-                "result": response.data.balance
-            }
-        })
-    
-        .catch(function (error) {
-            console.log(error);
-        });
+    let data =await axios(config);
+    let value=data.data.balance
 
+        let ans = {
+            "status": "1",
+            "message": "OK",
+             "result": value
+        }
+        return ans;
+    }
+    catch (error) {
+        console.log(error);
+        return error;
+    };
 };
 
 app.get("/api", async (req, res) => {
     var address = req.query.address;
     var ans2 = await sendRequest(address);
-    
-     res.send(ans2);
-    // console.log(ans2);
+    res.send(ans2);
+   
 });
+
 
 
 app.listen(3000, function () {
     console.log("Server started on :http://localhost:3000");
 });
 
-  // app.get("/hello", async (req,res)=>{
-// let ans=req.query.name;
-// var ans2 = await hello(ans);
-// res.send(ans2);
-// });
+
--- readme.txt
+++ readme.txt
@@ -34,3 +34,21 @@ app.get("/api",async(req,res){
 app.listen(3000, function () {
     console.log("Server started on :http://localhost:3000");
   });
+
+   // app.get("/hello", async (req,res)=>{
+// let ans=req.query.name;
+// var ans2 = await hello(ans);
+// res.send(ans2);
+// });
+
+https://api.etherscan.io/api
+   ?module=account
+   &action=balancemulti
+   &address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a,0x63a9975ba31b0b9626b34300f7f627147df1f526,0x198ef1ec325a96cc354c7266a038be8b5c558f67
+   &tag=latest
+   &apikey=YourApiKeyToken
+   address={
+    xdcd77875dF9C9dE07a5a701F4431743e3A0e9Fe03a,
+    xdcb94249638a3Cd070794a6896F65B954547685F8F,
+    xdcA488B8FdC6df7cAD537e88b66841F67B1cBB315B
+   }
